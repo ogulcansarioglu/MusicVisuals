1d585d8dca2bd47d5c8b83c4f6e9ad854c950cb3
ogulcansarioglu <75083740+berivanucan@users.noreply.github.com>
2022-03-27 18:24:51 +0100

    first iteration, procedurally created terrain with random colors and the inital music

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,21 +1,19 @@
 package ie.tudublin;
 
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
-
 public class Main
-{	
+{
+    
 
-	public void startUI()
+    public static void myVisual()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
-	}
+        processing.core.PApplet.runSketch( a, new Visual());
+    }
 
-	public static void main(String[] args)
-	{
-		Main main = new Main();
-		main.startUI();			
-	}
+   
+    public static void main(String[] args)
+    {
+        myVisual();
+        
+    }
 }
--- java/src/ie/tudublin/Visual.java
+++ java/src/ie/tudublin/Visual.java
@@ -1,146 +1,131 @@
+
+
 package ie.tudublin;
 
-import processing.core.PApplet;
-import ddf.minim.*;
+import java.io.Console;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import ddf.minim.analysis.BeatDetect;
 import ddf.minim.analysis.FFT;
-
-public abstract class Visual extends PApplet
-{
-	private int frameSize = 512;
-	private int sampleRate = 44100;
-
-	private float[] bands;
-	private float[] smoothedBands;
-
-	private Minim minim;
-	private AudioInput ai;
-	private AudioPlayer ap;
-	private AudioBuffer ab;
-	private FFT fft;
-
-	private float amplitude  = 0;
-	private float smothedAmplitude = 0;
-
-	
-	
-	public void startMinim() 
-	{
-		minim = new Minim(this);
-
-		fft = new FFT(frameSize, sampleRate);
-
-		bands = new float[(int) log2(frameSize)];
-  		smoothedBands = new float[bands.length];
-
-	}
-
-	float log2(float f) {
-		return log(f) / log(2.0f);
-	}
-
-	protected void calculateFFT() throws VisualException
-	{
-		fft.window(FFT.HAMMING);
-		if (ab != null)
-		{
-			fft.forward(ab);
-		}
-		else
-		{
-			throw new VisualException("You must call startListening or loadAudio before calling fft");
-		}
-	}
-
-	
-	public void calculateAverageAmplitude()
-	{
-		float total = 0;
-		for(int i = 0 ; i < ab.size() ; i ++)
-        {
-			total += abs(ab.get(i));
-		}
-		amplitude = total / ab.size();
-		smothedAmplitude = PApplet.lerp(smothedAmplitude, amplitude, 0.1f);
-	}
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PShape;
+import processing.opengl.PShader;
 
 
-	protected void calculateFrequencyBands() {
-		for (int i = 0; i < bands.length; i++) {
-			int start = (int) pow(2, i) - 1;
-			int w = (int) pow(2, i);
-			int end = start + w;
-			float average = 0;
-			for (int j = start; j < end; j++) {
-				average += fft.getBand(j) * (j + 1);
-			}
-			average /= (float) w;
-			bands[i] = average * 5.0f;
-			smoothedBands[i] = lerp(smoothedBands[i], bands[i], 0.05f);
-		}
-	}
-
-	public void startListening()
-	{
-		ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
-		ab = ai.left;
-	}
-
-	public void loadAudio(String filename)
-	{
-		ap = minim.loadFile(filename, frameSize);
-		ab = ap.mix;
-	}
-
-	public int getFrameSize() {
-		return frameSize;
-	}
-
-	public void setFrameSize(int frameSize) {
-		this.frameSize = frameSize;
-	}
-
-	public int getSampleRate() {
-		return sampleRate;
-	}
-
-	public void setSampleRate(int sampleRate) {
-		this.sampleRate = sampleRate;
-	}
-
-	public float[] getBands() {
-		return bands;
-	}
-
-	public float[] getSmoothedBands() {
-		return smoothedBands;
-	}
-
-	public Minim getMinim() {
-		return minim;
-	}
-
-	public AudioInput getAudioInput() {
-		return ai;
-	}
 
 
-	public AudioBuffer getAudioBuffer() {
-		return ab;
-	}
+public class Visual extends PApplet {
 
-	public float getAmplitude() {
-		return amplitude;
-	}
 
-	public float getSmoothedAmplitude() {
-		return smothedAmplitude;
-	}
+/**
+   *
+   */
+private static final String LUDENS_MP3 = "C:/Users/Olci/Desktop/OLCI/MusicVisuals/java/src/ie/tudublin/Ludens.mp3";
+int cols, rows;
+int scale = 10;
+int w = 2000;
+int h = 1600;
 
-	public AudioPlayer getAudioPlayer() {
-		return ap;
-	}
+Minim minim;
 
-	public FFT getFFT() {
-		return fft;
-	}
+AudioPlayer ap;
+AudioInput ai;
+AudioBuffer ab;
+BeatDetect bd;
+
+PShape vertex1, vertex2; 
+
+float flying = 0;
+
+float[][] terrain;
+
+
+public void settings() {
+  size(1000, 1000, P3D);
+  cols = w / scale;
+  rows = h/ scale;
+  terrain = new float[cols][rows];
+ 
+}
+
+public void setup(){
+
+    colorMode(RGB);
+   
+    minim = new Minim(this);
+    ap = minim.loadFile(LUDENS_MP3);
+    bd = new BeatDetect(ap.bufferSize(), ap.sampleRate());
+    ap.play();
+    ab = ap.mix;
+    bd.detect(ap.mix);
+
+
+}
+
+
+public void draw() {
+
+  flying -= 0.05;
+  float yoff = flying;
+
+  if (bd.isSnare()) {
+   System.out.print("obj");
+  } else {
+    System.out.print("1");
+  }
+
+
+  for (int y = 0; y < rows; y++) {
+    float xoff = 0;
+    for (int x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+  
+      xoff += 0.1;
+      noStroke();
+	 
+    }
+    yoff += 0.1;
+
+  
+  }
+
+
+
+ 
+  stroke(50);
+  beginShape(TRIANGLE_STRIP);
+
+
+
+  translate(width/2, height/2+50);
+  rotateX(PI/3);
+  background(0);
+  translate(-w/2, -h/2);
+  translate(mouseX, mouseY);
+ 
+  for (int y = 0; y < rows-1; y++) {
+    
+    for (int x = 0; x < cols; x++) {
+      vertex(x*scale, y*scale, terrain[x][y]);
+      vertex(x*scale, (y+1)*scale, terrain[x][y+1]);
+      fill(color(map(mouseX, 0, width, 10, 50)));
+      stroke(x * 2 , y * 2 , 255);
+      
+    
+     
+      //rect(x*scl, y*scl, scl, scl);
+    }
+    //stroke(color(map(mouseY, 0, 1, 0, 255)));
+   
+  
+
+  }
+  endShape();
+}
+
+    
 }
