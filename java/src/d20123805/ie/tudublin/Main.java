9fdec0cbf8667c02c38e598547bc6c1238145aed
ogulcansarioglu <ogulcansarioglu1@gmail.com>
2022-05-05 18:50:54 +0100

    the relative path names established with package name based on my student number

---
+++ java/src/d20123805/ie/tudublin/Main.java
@@ -0,0 +1,44 @@
+package d20123805.ie.tudublin;
+
+
+public class Main
+{
+
+    
+    
+
+    public static void myVisual()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Visual());
+    }
+
+   
+    public static void main(String[] args)
+    {
+
+
+
+        userInterface myUserInterface = new userInterface();
+
+        try {
+  
+      
+       
+        while(myUserInterface.getClicks() != 3) 
+        {
+          
+         System.out.println(myUserInterface.getClicks());
+            
+        }
+       
+        myVisual();
+    }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+        
+        
+    }
+}
